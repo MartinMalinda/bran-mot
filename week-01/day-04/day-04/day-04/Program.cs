86f186859b3123a506e8fc7b103c93857da55389
Brandon Motwani <brandon.motwani@gmail.comgit config --global user.email brandon.motwani@gmail.com>
2019-09-15 12:36:00 +0200

    fixed the pyramid program to render correctly

--- week-01/day-04/day-04/day-04/Program.cs
+++ week-01/day-04/day-04/day-04/Program.cs
@@ -7,41 +7,39 @@ namespace GreenFox
         static void Main(string[] args)
         {
             // Write a program that reads a number from the standard input, then draws a
-            // square like this:
+            // pyramid like this:
             //
             //
-            // %%%%%%
-            // %    %
-            // %    %
-            // %    %
-            // %    %
-            // %%%%%%
+            //    *
+            //   ***
+            //  *****
+            // *******
             //
-            // The square should have as many lines as the number was
+            // The pyramid should have as many lines as the number was
+            Console.WriteLine("How big of a pyramid do you want to make?");
+            int userNumber = int.Parse(Console.ReadLine());
+            string star = "*";
+            string blank = "-";
 
-            Console.WriteLine("Please enter a number for the width and height of a square: ");
-            int num = int.Parse(Console.ReadLine());
 
-            for (int i = 1; i <= num; i++)
+            for (int i = 1; i <= userNumber; i++)
             {
-                if (i == 1 || i == num)
+                //render spaces
+                for (int j = userNumber - i; j >= 1; j--)
                 {
-                    for (int j = 0; j < num; j++)
-                    {
-                        Console.Write("%");
-                    }
+                    Console.Write(" ");
+
                 }
-                else
+
+                //2*i-1 - basically calculate the number of x's in RELATIONSHIP to i
+
+                for (int k = 1; k <= 2*i-1; k++)
                 {
-                    Console.Write("%");
-                    for (int k = 0; k < num -2; k++)
-                    {
-                        Console.Write(" ");
-                    }
-                    Console.Write("%");
+                    Console.Write("*");
                 }
                 Console.WriteLine();
             }
+
         }
     }
 }
