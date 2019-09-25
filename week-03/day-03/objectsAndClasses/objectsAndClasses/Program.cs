53e824e134a5499291e08f83f97273ad59360d81
Brandon Motwani <brandon.motwani@gmail.comgit config --global user.email brandon.motwani@gmail.com>
2019-09-25 14:11:21 +0200

    solved Pokemon exercise, added as Program.cs, added supporting Pokemon class

---
+++ week-03/day-03/objectsAndClasses/objectsAndClasses/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace objectsAndClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Pokemon> pokemonOfAsh = InitializePokemon();
+
+            // Every pokemon has a name and a type.
+            // Certain types are effective against others, e.g. water is effective against fire.
+
+            // Ash has a few pokemon.
+            // A wild pokemon appeared!
+
+            Pokemon wildPokemon = new Pokemon("Oddish", PokemonType.Leaf, PokemonType.Water);
+            string pokemonToUse = "";
+
+            // Which pokemon should Ash use?
+            foreach (var pokemon in pokemonOfAsh)
+            {
+                if (pokemon.IsEffectiveAgainst(wildPokemon))
+                {
+                    pokemonToUse = pokemon.Name;
+                }
+            }
+
+            Console.Write($"I choose you, {pokemonToUse}");
+        }
+
+        private static List<Pokemon> InitializePokemon()
+        {
+            return new List<Pokemon>
+            {
+                new Pokemon("Balbasaur", PokemonType.Leaf, PokemonType.Water),
+                new Pokemon("Pikatchu", PokemonType.Electric, PokemonType.Water),
+                new Pokemon("Charizard", PokemonType.Fire, PokemonType.Leaf),
+                new Pokemon("Balbasaur", PokemonType.Water, PokemonType.Fire),
+                new Pokemon("Kingler", PokemonType.Water, PokemonType.Fire)
+            };
+        }
+    }
+}
