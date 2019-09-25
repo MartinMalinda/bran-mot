b8e3c1289d498f56f6c9c3941f4e7f73c7c59884
Brandon Motwani <brandon.motwani@gmail.comgit config --global user.email brandon.motwani@gmail.com>
2019-09-25 12:04:22 +0200

    added blog post exercise.

---
+++ week-03/day-03/objectsAndClasses/objectsAndClasses/BlogPost.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace objectsAndClasses
+{
+    public class BlogPost
+    {
+        //Create a BlogPost class that has
+        //an authorName
+        //a title
+        //a text
+        //a publicationDate
+
+        public string authorName { get; set; }
+        public string title {get; set;}
+        public string publicationDate { get; set; }
+        public string text { get; set; }
+
+        public BlogPost()
+        {
+        }
+        public BlogPost(string author, string title, string published, string content)
+        {
+            this.authorName = author;
+            this.title = title;
+            this.publicationDate = published;
+            this.text = content;
+        }
+    }
+}
--- week-03/day-03/objectsAndClasses/objectsAndClasses/Program.cs
+++ week-03/day-03/objectsAndClasses/objectsAndClasses/Program.cs
@@ -6,7 +6,36 @@ namespace objectsAndClasses
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            //Create a few blog post objects:
+            //"Lorem Ipsum" titled by John Doe posted at "2000.05.04."
+            //Lorem ipsum dolor sit amet.
+            //"Wait but why" titled by Tim Urban posted at "2010.10.10."
+            //A popular long-form, stick-figure-illustrated blog about almost everything.
+            //"One Engineer Is Trying to Get IBM to Reckon With Trump" titled by William Turton at "2017.03.28."
+            //Daniel Hanley, a cybersecurity engineer at IBM, doesn’t want to be the center of attention.When I asked to take his picture outside one of IBM’s New York City offices, he told me that he wasn’t really into the whole organizer profile thing.
+
+            BlogPost Lorem = new BlogPost(
+                "John Doe",
+                "Lorem Ipsum",
+                "2000.05.04",
+                "Lorem ipsum dolor sit amet."
+                );
+            BlogPost WaitButWhy = new BlogPost(
+                "Wait but why",
+                "Tim Urban",
+                "2010.10.10",
+                "A popular long-form, stick-figure-illustrated blog about almost everything."
+                );
+            BlogPost OneEngineer = new BlogPost(
+                "One Engineer Is Trying to Get IBM to Reckon With Trump",
+                "William Turton",
+                "2017.03.28",
+                "Daniel Hanley, a cybersecurity engineer at IBM, doesn’t want to be the center of attention.When I asked to take his picture outside one of IBM’s New York City offices, he told me that he wasn’t really into the whole organizer profile thing."
+                );
+
+
+
         }
     }
 }
