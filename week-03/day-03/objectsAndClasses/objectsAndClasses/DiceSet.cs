feef9bfac8207ba2e260e4024339ab3908619de1
Brandon Motwani <brandon.motwani@gmail.comgit config --global user.email brandon.motwani@gmail.com>
2019-09-25 16:01:27 +0200

    updated Program.cs with dice exercise, added dice class

---
+++ week-03/day-03/objectsAndClasses/objectsAndClasses/DiceSet.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace objectsAndClasses
+{
+    public class DiceSet
+    {
+        public DiceSet()
+        {
+        }
+    }
+}
--- week-03/day-03/objectsAndClasses/objectsAndClasses/Program.cs
+++ week-03/day-03/objectsAndClasses/objectsAndClasses/Program.cs
@@ -7,38 +7,35 @@ namespace objectsAndClasses
     {
         public static void Main(string[] args)
         {
-            var fleet = new Fleet();
-            // - You have the `Thing` class
-            // - You have the `Fleet` class
-            // - You have the `FleetOfThings` class with a `Main` method
-            // - Download those, use those
-            // - In the Main method create a fleet
-            // - Achieve this output:s
-            // Create a fleet of things to have this output:
-            // 1. [ ] Get milk
-            // 2. [ ] Remove the obstacles
-            // 3. [x] Stand up
-            // 4. [x] Eat lunch
-            // Hint: You have to create a Print method also
+            // You have a `DiceSet` class which has a list for 6 dice
+            // You can roll all of them with roll()
+            // Check the current rolled numbers with getCurrent()
+            // You can reroll with reroll()
+            // Your task is to roll the dice until all of the dice are 6
+            DiceSet diceSet = new DiceSet();
 
-            string[] thingsToAdd = { "Get milk", "Remove the obstacles", "Stand up", "Eat lunch" };
-            Thing[] thingsForFleet = new Thing[thingsToAdd.Length];
-
-            for (int i = 0; i < thingsToAdd.Length; i++)
+            diceSet.Roll();
+            foreach (var value in diceSet.GetCurrent())
             {
-                thingsForFleet[i] = new Thing(thingsToAdd[i]);
+                Console.Write($"{value} ");
             }
-            thingsForFleet[2].Complete();
-            thingsForFleet[3].Complete();
+            Console.WriteLine();
 
-            foreach (var thing in thingsForFleet)
+            for (int i = 0; i < diceSet.GetCurrent().Length; i++)
             {
-                fleet.Add(thing);
+                if (diceSet.GetCurrent(i) != 6)
+                {
+                    do
+                    {
+                        diceSet.Reroll(i);
+                        foreach (var value in diceSet.GetCurrent())
+                        {
+                            Console.Write($"{value} ");
+                        }
+                        Console.WriteLine();
+                    } while (diceSet.GetCurrent(i) != 6);
+                }
             }
-
-            fleet.PrintFleet();
-            
         }
-    
     }
 }
