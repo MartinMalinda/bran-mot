6a6912e9ba652f9d85a7080bcb1066693bf3e5ba
Brandon Motwani <brandon.motwani@gmail.comgit config --global user.email brandon.motwani@gmail.com>
2019-09-26 16:18:08 +0200

    added blog exercise, updated Program.cs

--- week-03/day-03/objectsAndClasses/objectsAndClasses/Program.cs
+++ week-03/day-03/objectsAndClasses/objectsAndClasses/Program.cs
@@ -7,22 +7,38 @@ namespace objectsAndClasses
     {
         public static void Main(string[] args)
         {
-            Animal cheetah = new Animal();
-            Animal turkey = new Animal();
-            Animal bear = new Animal();
+            BlogPost Lorem = new BlogPost(
+                "John Doe",
+                "Lorem Ipsum",
+                "2000.05.04",
+                "Lorem ipsum dolor sit amet."
+                );
+            BlogPost WaitButWhy = new BlogPost(
+                "Tim Urban",
+                "Wait but why",
+                "2010.10.10",
+                "A popular long-form, stick-figure-illustrated blog about almost everything."
+                );
+            BlogPost OneEngineer = new BlogPost(
+                "William Turton",
+                "One Engineer Is Trying to Get IBM to Reckon With Trump",
+                "2017.03.28",
+                "Daniel Hanley, a cybersecurity engineer at IBM, doesn’t want to be the center of attention.When I asked to take his picture outside one of IBM’s New York City offices, he told me that he wasn’t really into the whole organizer profile thing."
+                );
 
-            Farm myFarm = new Farm(20);
+            Blog myBlog = new Blog();
 
-            myFarm.AddAnimal(cheetah);
-            myFarm.AddAnimal(turkey);
-            myFarm.AddAnimal(bear);
+            myBlog.AddBlogPost(Lorem);
+            myBlog.AddBlogPost(WaitButWhy);
+            myBlog.AddBlogPost(OneEngineer);
+            Console.WriteLine();
+            Console.WriteLine("My Blog so Far:");
+            myBlog.PrintTitles();
 
-            myFarm.Breed(turkey);
+            myBlog.Update(1, Lorem);
 
-            bear.Eat();
-            bear.Eat();
-
-            myFarm.Slaughter();
+            Console.WriteLine();
+            myBlog.PrintTitles();
         }
     }
 }
