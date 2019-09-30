6d2e04b65e6f19a9cbdbf95d5ad0b0ed0eb4ad3c
Brandon Motwani <brandon.motwani@gmail.comgit config --global user.email brandon.motwani@gmail.com>
2019-09-30 10:12:52 +0200

    added most common characters exercise

---
+++ week-04/day-01/mostCommonCharacters.cs
@@ -0,0 +1,47 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace trialExam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MostCommonCharacters("./countchar.txt")); 
+        }
+        public static string MostCommonCharacters(string filename)
+        {
+            string fileContent = File.ReadAllText(filename);
+            fileContent.ToCharArray();
+            Dictionary<char, int> characterDictionary = new Dictionary<char, int>();
+            string retrunString = "";
+            foreach (var character in fileContent)
+            {
+                if (characterDictionary.ContainsKey(character))
+                {
+                    characterDictionary[character]++;
+                }
+                else
+                {
+                    characterDictionary.Add(character, 1);
+                }
+            }
+            int[] characterCounts = new int[characterDictionary.Count];
+            char[] characters = new char[characterDictionary.Count];
+            int counter = 0;
+            foreach (KeyValuePair<char, int> item in characterDictionary)
+            {
+                characters[counter] = item.Key;
+                characterCounts[counter] = item.Value;
+                counter++;
+            }
+            Array.Sort(characterCounts, characters);
+            for (int i = characters.Length-1; i > characters.Length-3; i--)
+            {
+                retrunString = retrunString + characters[i] +": "+ characterCounts[i] +"\n";
+            }
+            return retrunString;
+        }
+    }
+}
