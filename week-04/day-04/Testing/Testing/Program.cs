28eb0f03d67763fc0b03d3b7cf3cba6289bd0359
Brandon Motwani <brandon.motwani@gmail.comgit config --global user.email brandon.motwani@gmail.com>
2019-10-03 11:17:36 +0200

    Added apple test and program

---
+++ week-04/day-04/Testing/Testing/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Testing
+{
+    //Create a class, with one method(eg. public string GetApple()) that returns a string (eg. "apple")
+    //Create a test for that.
+    //Create an xUnit project
+    //Add a new Test case in it
+
+    public class Apple
+    {
+        public string Name { get; set; }
+
+        public Apple(string name)
+        {
+            Name = name;
+        }
+
+        public string GetApple()
+        {
+            return Name;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ week-04/day-04/Testing/myTests/UnitTest1.cs
@@ -0,0 +1,27 @@
+using System;
+using Xunit;
+using Testing;
+
+namespace myTests
+{
+    public class UnitTest1
+    {
+        //Instantiate your class (arrange, act, assert)
+        //Use the Assert.Equal()
+        //The expected parameter should be the same string (eg. "apple")
+        //The actual parameter should be the return value of the method(eg.myObject.GetApple())
+        //Run the test
+        //Change the expected value to make the test failing
+        //Run the test
+        //Fix the returned value to make the test succeeding again
+        [Fact]
+        public void Test1()
+        {
+            Apple redApple = new Apple("Red Delicious");
+
+            //Assert.Equal("Red Disgusting", redApple.GetApple());
+            Assert.Equal("Red Delicious", redApple.GetApple());
+
+        }
+    }
+}
