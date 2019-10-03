31a091a01730acf720878f129d7b9263a85855fe
Brandon Motwani <brandon.motwani@gmail.comgit config --global user.email brandon.motwani@gmail.com>
2019-10-03 13:14:28 +0200

    Updated tests and program for the anagram exercise

--- week-04/day-04/Testing/Testing/Program.cs
+++ week-04/day-04/Testing/Testing/Program.cs
@@ -3,31 +3,44 @@ using System.Collections.Generic;
 
 namespace Testing
 {
-    public class MathOperations
+    //Write a function, that takes two strings and returns a boolean value based on if the two strings are Anagramms or not.
+    public class AnagramChecker
     {
-        public MathOperations() { }
-        public int SumAll(List<int> numbersToSum)
-        {
-            int sum = 0;
+        public AnagramChecker() { }
 
-            foreach (var number in numbersToSum)
+        public bool Anagram(string string1, string string2)
+        {
+            char[] chars1 = string1.ToCharArray();
+            char[] chars2 = string2.ToCharArray();
+            Array.Sort(chars1);
+            Array.Sort(chars2);
+
+            if (chars1.Length != chars2.Length)
             {
-                sum += number;
+                return false;
             }
-
-            return sum;
-        }
-        public int? SumAll(List<int?> numbersToSum)
-        {
-            int? sum = null;
-            return sum;
+            for (int i = 0; i < chars1.Length; i++)
+            {
+                if (chars1[i] != chars2[i])
+                {
+                    return false;
+                }
+            }
+            return true;
         }
     }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //char[] charsOne = { 'd', 'o', 'g' };
+            //char[] charsTwo = { 'g', 'o', 'd' };
+            //Array.Sort(charsTwo);
+            //Array.Sort(charsOne);
+
+
         }
     }
 }
+
--- week-04/day-04/Testing/myTests/UnitTest1.cs
+++ week-04/day-04/Testing/myTests/UnitTest1.cs
@@ -10,47 +10,15 @@ namespace myTests
         [Fact]
         public void Test1()
         {
-            List<int> myNumbers= new List<int>();
-            myNumbers.Add(1);
-            myNumbers.Add(2);
-            myNumbers.Add(3);
-
-            MathOperations mySum = new MathOperations();
-
-            Assert.Equal(6, mySum.SumAll(myNumbers));    
+            AnagramChecker myAnagram = new AnagramChecker();
+            Assert.True(myAnagram.Anagram("dog", "god"));
         }
 
         [Fact]
-        public void TestEmptyList()
+        public void Test2()
         {
-            List<int> myNumbers = new List<int>();
-
-            MathOperations mySum = new MathOperations();
-
-            Assert.Equal(0, mySum.SumAll(myNumbers));
-        }
-
-        [Fact]
-        public void TestOneItemList()
-        {
-            List<int> myNumbers = new List<int>();
-            myNumbers.Add(1);
-
-            MathOperations mySum = new MathOperations();
-
-            Assert.Equal(1, mySum.SumAll(myNumbers));
-        }
-
-        [Fact]
-        public void TestNullList()
-        {
-            int? nullInt = null;
-            List<int?> myNumbers = new List<int?>();
-            myNumbers.Add(nullInt);
-
-            MathOperations mySum = new MathOperations();
-
-            Assert.Equal(null, mySum.SumAll(myNumbers));
+            AnagramChecker myAnagram = new AnagramChecker();
+            Assert.True(myAnagram.Anagram("hello", "go away"));
         }
     }
 }
