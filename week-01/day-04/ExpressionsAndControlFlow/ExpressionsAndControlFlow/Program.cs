f0238ca58f700b4c9627ea4a63dc7fd3fb93c913
Brandon Motwani <brandon.motwani@gmail.comgit config --global user.email brandon.motwani@gmail.com>
2019-10-09 13:56:08 +0200

    Add - Program files - W1 - D4

---
+++ week-01/day-04/ExpressionsAndControlFlow/ExpressionsAndControlFlow/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace ExpressionsAndControlFlow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that reads a number from the standard input, then draws a
+            // diamond like this:
+            //
+            //
+            // 1 ---*
+            // 2 --***
+            // 3 -*****
+            // 4 *******
+            // 5 -***** 
+            // 6 --***  
+            // 7 ---*
+
+            Console.WriteLine("How tall of a pyramid do you want to create?");
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            int halfwayPoint = num/2;
+            if (num % 2 != 0)
+            {
+                for (int rows = 1; rows <= halfwayPoint; rows++)
+                {
+                    for (int spaces = halfwayPoint - rows+1; spaces > 0; spaces--)
+                    {
+                        Console.Write(" ");
+                    }
+                    for (int stars = 0; stars < (rows * 2) - 1; stars++)
+                    {
+                        Console.Write("*");
+                    }
+                    Console.WriteLine();
+                }
+                for (int rows = 0; rows <= halfwayPoint+1; rows++)
+                {
+                    for (int spaces = rows; spaces > 0; spaces--)
+                    {
+                        Console.Write(" ");
+                    }
+                    for (int stars = 0; stars < num - (2 * rows); stars++)
+                    {
+                        Console.Write("*");
+                    }
+                    Console.WriteLine();
+                }
+            }
+            else
+            {
+                for (int rows = 1; rows <= halfwayPoint; rows++)
+                {
+                    for (int spaces = halfwayPoint - rows; spaces > 0; spaces--)
+                    {
+                        Console.Write(" ");
+                    }
+                    for (int stars = 0; stars < (rows * 2) - 1; stars++)
+                    {
+                        Console.Write("*");
+                    }
+                    Console.WriteLine();
+                }
+                for (int rows = 0; rows <= halfwayPoint; rows++)
+                {
+                    for (int spaces = rows; spaces > 0; spaces--)
+                    {
+                        Console.Write(" ");
+                    }
+                    for (int stars = 1; stars < num - (2 * rows); stars++)
+                    {
+                        Console.Write("*");
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
