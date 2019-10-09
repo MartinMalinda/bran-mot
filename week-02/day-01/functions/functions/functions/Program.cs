ad2b079c1487f3a282976c982c802696efd952b1
Brandon Motwani <brandon.motwani@gmail.comgit config --global user.email brandon.motwani@gmail.com>
2019-10-09 13:53:28 +0200

    Fix - Functions - W2 - D1

--- week-02/day-01/functions/functions/functions/Program.cs
+++ week-02/day-01/functions/functions/functions/Program.cs
@@ -4,35 +4,76 @@ namespace GreenFox
 {
     class Program
     {
-        public static string Palindrome(string word)
-        {
-            string palindrome = "";
-            char[] letters = word.ToCharArray();
-            char[] mirroredLetters = new char[word.Length];
-            for (int i = 0; i < letters.Length; i++)
-            {
-                mirroredLetters[i] = letters[i];
-            }
-            Array.Reverse(mirroredLetters);
-            foreach (var letter in letters )
-            {
-                palindrome = palindrome + letter;
-            }
-            foreach (var letter in mirroredLetters)
-            {
-                palindrome = palindrome + letter;
-            }
-            return palindrome;
-        }
-        
+        //  Create a function that takes a list of numbers as parameter
+        //  Returns a list where the elements are sorted in ascending numerical order
+        //  Make a second boolean parameter, if it's `true` sort that list descending
+
         static void Main(string[] args)
         {
-            Console.WriteLine(Palindrome("56788"));
+            //  Example:
+            Console.WriteLine(Bubble(new int[] { 34, 12, 24, 9, 5 }));
+            //  should print [5, 9, 12, 24, 34]
+            Console.WriteLine(AdvancedBubble(new int[] { 34, 12, 24, 9, 5 }, true));
+            //  should print [34, 24, 12, 9, 5]
         }
-        //multiply the length by 2
-        //initialize the values with one for loop
-        //.length - i - 1
-        //should only use one for loop
 
+        public static string Bubble(int[] list)
+        {
+            int swap = 0;
+            string sorted = "";
+            for (int i = 0; i < list.Length; i++)
+            {
+                for (int j = i+1; j < list.Length; j++)
+                {
+                    if (list[i] > list[j])
+                    {
+                        swap = list[i];
+                        list[i] = list[j];
+                        list[j]  = swap;
+                    }
+                    swap = 0;
+                }
+            }
+            sorted = "[";
+            foreach (var item in list)
+            {
+                sorted += item + ", ";
+            }
+            sorted += "]";
+            return sorted;
+        }
+
+        public static string AdvancedBubble(int[] list, bool descending)
+        {
+            if (descending)
+            {
+                int swap = 0;
+                string sorted = "";
+                for (int i = 0; i < list.Length; i++)
+                {
+                    for (int j = i + 1; j < list.Length; j++)
+                    {
+                        if (list[i] < list[j])
+                        {
+                            swap = list[i];
+                            list[i] = list[j];
+                            list[j] = swap;
+                        }
+                        swap = 0;
+                    }
+                }
+                sorted = "[";
+                foreach (var item in list)
+                {
+                    sorted += item + ", ";
+                }
+                sorted += "]";
+                return sorted;
+            }
+            else{
+                return "error";
+            }
+            
+        }
     }
 }
