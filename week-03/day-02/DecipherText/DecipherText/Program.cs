349cacd5b1dc65d78980ca5241615020edb31939
Brandon Motwani <brandon.motwani@gmail.comgit config --global user.email brandon.motwani@gmail.com>
2019-10-10 18:55:22 +0200

    Add - decipher text - W3 - D2

---
+++ week-03/day-02/DecipherText/DecipherText/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace DecipherText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(StarReplace("hetjdtu", 0));
+        }
+
+        //Given a string, compute recursively a new string where all the adjacent chars are now separated by a*
+
+        public static string StarReplace(string input, int n)
+        {
+            if (n != input.Length - 1)
+            {
+                return input[n] + "*" + StarReplace(input, n + 1);
+            }
+            return input[n] + "";
+        }
+    }
+}
