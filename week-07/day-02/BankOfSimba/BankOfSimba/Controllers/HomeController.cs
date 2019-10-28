01e9a91442321fa689bda5ff9e5dffe684c27dc4
Brandon Motwani <brandon.motwani@gmail.comgit config --global user.email brandon.motwani@gmail.com>
2019-10-28 13:18:07 +0100

    Add - Bank of simba progress - W7 - D2

---
+++ week-07/day-02/BankOfSimba/BankOfSimba/Controllers/HomeController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BankOfSimba.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BankOfSimba.Controllers
+{
+    
+    public class HomeController : Controller
+    {
+
+        public static List<BankAccount> AllAccounts { get; set; } = new List<BankAccount>
+        {
+            new BankAccount("Timon", 50000.00, "Meerkat", true),
+            new BankAccount("Poomba", 0.01, "Warthog"),
+            new BankAccount("Ariel", 5.03, "Mermaid"),
+            new BankAccount("Mulan", 25.43, "Human", false, true)
+        };
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("show")]
+        [HttpGet("")]
+        public IActionResult ShowSimba()
+        {
+            var simba = new BankAccount("Simba", 2000, "lion");
+            ViewBag.Item = simba;
+            return View();
+        }
+
+        [HttpGet("htmlception")]
+        public IActionResult htmlCeption()
+        {
+            ViewData["string"] = "This is an <em>HTML</em> text. <b>Enjoy yourself!</b>";
+            return View();
+        }
+        [HttpGet("all-accounts")]
+        public IActionResult allAccounts()
+        {
+            return View(AllAccounts);
+        }
+        [HttpPost("all-accounts")]
+        public IActionResult allAccounts(string name, double balance, string species, bool king, bool goodGuy)
+        {
+            AllAccounts.Add(new BankAccount(name, balance, species, king, goodGuy));
+            return View(AllAccounts);
+        }
+
+        [HttpPost("all-accounts")]
+        public IActionResult allAccounts(bool increase)
+        {
+            foreach (var account in AllAccounts)
+            {
+                if (account.IsKing)
+                {
+                    account.Balance += 1000;
+                }
+                else
+                {
+                    account.Balance += 10;
+                }
+            }
+            return View(AllAccounts);
+        }
+
+    }
+}
---
+++ week-07/day-02/BankOfSimba/BankOfSimba/Models/BankAccount.cs
@@ -0,0 +1,45 @@
+﻿using System;
+namespace BankOfSimba.Models
+{
+    public class BankAccount
+    {
+        public string Name { get; private set; }
+        public double Balance { get; set; }
+        public string AnimalType { get; private set; }
+        public bool IsKing { get; private set; } = false;
+        public bool IsGoodGuy { get; private set; } = true;
+
+        public BankAccount(string name, double balance, string animalType )
+        {
+            Name = name;
+            Balance = balance;
+            AnimalType = animalType;
+        }
+
+        public BankAccount(string name, double balance, string animalType, bool isKing)
+        {
+            Name = name;
+            Balance = balance;
+            AnimalType = animalType;
+            if (isKing)
+            {
+                IsKing = true;
+            }
+        }
+
+        public BankAccount(string name, double balance, string animalType, bool isKing, bool isEvil)
+        {
+            Name = name;
+            Balance = balance;
+            AnimalType = animalType;
+            if (isKing)
+            {
+                IsKing = true;
+            }
+            if (isEvil)
+            {
+                IsGoodGuy = false;
+            }
+        }
+    }
+}
