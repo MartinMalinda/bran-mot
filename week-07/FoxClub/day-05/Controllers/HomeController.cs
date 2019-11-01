8f7bfc01de6865b0d8a53c256246c04e05f57ed1
Brandon Motwani <brandon.motwani@gmail.comgit config --global user.email brandon.motwani@gmail.com>
2019-11-01 13:28:59 +0100

    Add - progress on fox club - W7 - D5

---
+++ week-07/FoxClub/day-05/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using day05.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace day05.Controllers
+{
+    [Route("")]
+    public class HomeController : Controller
+    {
+        private IFox FoxService;
+
+        public HomeController(IFox foxService)
+        {
+            FoxService = foxService;
+        }
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("")]
+        public IActionResult Index(string name)
+        {
+            return View(FoxService.GetCurrentFox());
+        }
+
+        [HttpGet("login")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login(string name)
+        {
+            FoxService.AddFox(name);
+            FoxService.ChangeFox(name);
+            return RedirectToAction("Index", "Home", new { name = FoxService.GetCurrentFox().Name});
+        }
+    }
+}
---
+++ week-07/FoxClub/day-05/Services/FoxService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using day05.Interfaces;
+using day05.Models;
+
+namespace day05.Services
+{
+    public class FoxService : IFox
+    {
+        public List<Fox> FoxList { get; private set; }
+        public Fox CurrentFox { get; private set; }
+
+        public FoxService()
+        {
+            Fox mrFox = new Fox("Mr. Fox");
+            mrFox.AddFood("Pizza");
+            mrFox.AddFood("Lemonade");
+            mrFox.AddTrick("Write HTML");
+            mrFox.AddTrick("Code in Java");
+
+            FoxList = new List<Fox>
+            {
+                mrFox
+            };
+            ChangeFox("Mr. Fox");
+        }
+
+        public void AddFox(string name)
+        {
+            FoxList.Add(new Fox(name));
+        }
+
+        public void ChangeFox(string name)
+        {
+            CurrentFox = FoxList.First(fox => name == fox.Name);
+        }
+
+        public void CheckFox(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Fox GetCurrentFox()
+        {
+            return CurrentFox;
+        }
+    }
+}
